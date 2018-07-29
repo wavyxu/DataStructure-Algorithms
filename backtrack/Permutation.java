159641c7fffa3a77f05725230af0db8bb5b1da93
Wei Xu <wavyxu@gmail.com>
2018-07-29 14:19:40 -0700

    Add file about permutation

---
+++ backtrack/Permutation.java
@@ -0,0 +1,54 @@
+package backtrack;
+import java.util.*;
+
+/**
+ * Given a collection of distinct integers, return all possible permutations.
+ *
+ * Example:
+ *
+ * Input: [1,2,3]
+ * Output:ums, 0, new ArrayList<Integer>(), res);
+        return res;
+ private void
+    }
+
+ * [
+ *   [1,2,3],
+ *   [1,3,2],
+ *   [2,1,3],
+ *   [2,3,1], helper(int[] nums, int start, List<Integer> prefix, List<List<Integer>> res) {
+ *  *  *         if (prefix.size() == nums.length) {
+ *  *  *             res.add(new ArrayList<>(prefix));
+ *  *  *             return;
+ *  *  *         }
+ *   [3,1,2],
+ *   [3,2,1]
+ * ]
+ */
+public class Permutation {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(nums, new ArrayList<>(), res);
+        return res;
+    }
+
+
+    private void helper(int[] nums, List<Integer> prefix, List<List<Integer>> res) {
+
+        if (prefix.size() == nums.length) {
+            res.add(new ArrayList<>(prefix));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+                if (prefix.contains(nums[i])) {
+                    continue;
+                }
+
+                prefix.add(nums[i]);
+                helper(nums, prefix, res);
+                prefix.remove(prefix.size() - 1);
+        }
+    }
+}
+
