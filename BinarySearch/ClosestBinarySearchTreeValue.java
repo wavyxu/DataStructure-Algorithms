42e49bf1994a78f9e79f107e8347734e529b164e
Wei Xu <wavyxu@gmail.com>
2018-08-03 23:50:31 -0700

    ClosestBinarySearchTreeValue

--- BinarySearch/ClosestBinarySearchTreeValue.java
+++ BinarySearch/ClosestBinarySearchTreeValue.java
@@ -1,4 +1,4 @@
-package BinarySearch;
+package binarysearch;
 
 /**
  *Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
@@ -32,5 +32,19 @@ public class ClosestBinarySearchTreeValue {
         }
 
         return res;
+    }
+
+    // 利用二分法找出左右子树 中 最有可能 出现结果的子树，设定为目标子树
+    // 找出子树中最接近 target 的值
+    // 对比子树返回的结果，和自身 root.val 相比，谁更接近 target
+    // exit condition ： 碰到目标子树为空， 返回自己的 value
+    public int closestValueRecurive(TreeNode root, double target) {
+        int currVaule = root.val;
+        TreeNode nextRoot = currVaule > target ? root.left : root.right;
+        if (nextRoot == null) {
+            return root.val;
+        }
+        int nextValue = closestValueRecurive(nextRoot, target);
+        return Math.abs(currVaule - target) > Math.abs(nextValue - target) ? nextValue : currVaule;
     }
 }
