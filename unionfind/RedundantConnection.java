0f452ee131e87854d3bbbccb6eb9e8c3eb0049f4
Wei Xu <wavyxu@gmail.com>
2018-08-11 23:19:22 -0700

    Redundant connection

---
+++ unionfind/RedundantConnection.java
@@ -0,0 +1,92 @@
+package unionfind;
+import java.util.*;
+
+/**
+ * In this problem, a tree is an undirected graph that is connected and has no cycles.
+ *
+ * The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N),
+ * with one additional edge added. The added edge has two different vertices chosen from 1 to N,
+ * and was not an edge that already existed.
+ *
+ * The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] with u < v, that represents an undirected edge connecting nodes u and v.
+ *
+ * Return an edge that can be removed so that the resulting graph is a tree of N nodes. If there are multiple answers, return the answer that occurs last in the given 2D-array. The answer edge [u, v] should be in the same format, with u < v.
+ *
+ * Example 1:
+ * Input: [[1,2], [1,3], [2,3]]
+ * Output: [2,3]
+ * Explanation: The given undirected graph will be like this:
+ *   1
+ *  / \
+ * 2 - 3
+ * Example 2:
+ * Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
+ * Output: [1,4]
+ * Explanation: The given undirected graph will be like this:
+ * 5 - 1 - 2
+ *     |   |
+ *     4 - 3
+ * Note:
+ * The size of the input 2D-array will be between 3 and 1000.
+ * Every integer represented in the 2D-array will be between 1 and N, where N is the size of the input array.
+ */
+public class RedundantConnection {
+    class UnionFind {
+        private int[] father;
+        private int count;
+        public UnionFind(int n) {
+            father = new int[n];
+            for (int i = 0; i < n; i++) {
+                father[i] = i;
+            }
+            count = n;
+        }
+
+        public int find(int x) {
+            if (father[x] == x) {
+                return x;
+            }
+            return father[x] = find(father[x]);
+        }
+
+        // non-recursive
+        public int find1(int x) {
+            List<Integer> path = new ArrayList<>();
+            while (father[x] != x) {
+                path.add(father[x]);
+                x = father[x];
+            }
+            for (Integer curr : path) {
+                father[curr] = x;
+            }
+            return x;
+        }
+
+        public void union(int a, int b) {
+            int rootA = find(a);
+            int rootB = find(b);
+            if (rootA != rootB) {
+                father[rootA] = rootB;
+                count--;
+            }
+        }
+
+        public int query() {
+            return count;
+        }
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n + 1);
+
+        for (int[] edge : edges) {
+            if (uf.find(edge[0]) == uf.find(edge[1])) {
+                return edge;
+            } else {
+                uf.union(edge[0], edge[1]);
+            }
+        }
+        return new int[0];
+    }
+}
